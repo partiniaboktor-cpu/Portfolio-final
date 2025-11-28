4407832a619dc82f2f129e2d86c2b1058460d0a7
Pattou Emad <pattouemad@Partinias-MacBook.local>
2025-11-28 13:57:13 +0200

    portfolio

--- src/pages/Articledetail.jsx
+++ src/pages/Articledetail.jsx
@@ -4,7 +4,6 @@ import PageTitle from '../components/PageTitle';
 import Subtitle from '../components/Subtitles';
 import Nav from '../components/Nav';
 import Footer from '../components/Footer';
-import { div } from 'framer-motion/client';
 
 const Article = () => {
     return ( <>
@@ -19,17 +18,55 @@ const Article = () => {
 <div className='sub-title'>
 <Subtitle  title="“Capturing moments that speak louder than words.”"></Subtitle>
 </div>
+    
+    
+    <h1 className='thes'>The ultimate guide for being an
+ UX/UI Designer</h1>
+    
+    <div className='bigart'>
+<div className='block1'>
+    <h2 className='hh'>1- Case study reflections</h2>
+    <p className='hhh'>Every project I design begins with understanding the problem from a user’s perspective. My case studies highlight the full journey — from initial research and wireframing to final interface design and testing. I focus on uncovering user pain points, transforming them into intuitive digital experiences that balance functionality with beauty. Each reflection showcases how thoughtful design decisions, guided by empathy and strategy, can create meaningful impact. Through these projects, I aim to demonstrate not only my visual skills but also my ability to think critically, collaborate effectively, and craft experiences that connect people with purpose.</p>
+</div>
+</div>
+
+
+    <div className='bigart'>
+<div className='block1'>
+    <h2 className='hh'>2. UX/UI Principles & Design Thinking</h2>
+    <p className='hhh'>Coming into the UX/UI principles & design thinking. My design approach is rooted into user experince and user interface principles and the design thinking process . Being a UX/UI designer have many principles User experience design (UX design, UXD, UED, or XD), upon which is the centralized requirements for "User Experience Design Research" (also known as UX Design Research), defines the experience a user would go through when interacting with a company, its services, and its products</p>
+</div>
+</div>
+
+
+        <div className='bigart'>
+<div className='block1'>
+    <h2 className='hh'>3.UI designer near Nasr City</h2>
+    <p className='hhh'>Near Nasr City , they might be many designers that hard are hard to be reached, but maybe through me it can be easy for their contact . UX/UI Designer in Nasr City gives you good access to Cairo’s business and tech ecosystem. This means as a designer you’re well-positioned to connect with both local clients and regional/remote work.
+The business-environment in Cairo (and especially vibrant areas like Nasr City) means being able to highlight local presence, fast communication, and cultural familiarity.</p>
+</div>
+</div>
+
+
+        <div className='bigart'>
+<div className='block1'>
+    <h2 className='hh'>3.UI designer near Nasr City</h2>
+    <p className='hhh'>Near Nasr City , they might be many designers that hard are hard to be reached, but maybe through me it can be easy for their contact . UX/UI Designer in Nasr City gives you good access to Cairo’s business and tech ecosystem. This means as a designer you’re well-positioned to connect with both local clients and regional/remote work.
+The business-environment in Cairo (and especially vibrant areas like Nasr City) means being able to highlight local presence, fast communication, and cultural familiarity.</p>
+</div>
+</div>
 
 
 
-
-
-
-
-
-
-
-
+        <div className='bigart'>
+<div className='block1'>
+    <h2 className='hh'>3.UI designer near Nasr City</h2>
+    <p className='hhh'>Near Nasr City , they might be many designers that hard are hard to be reached, but maybe through me it can be easy for their contact . UX/UI Designer in Nasr City gives you good access to Cairo’s business and tech ecosystem. This means as a designer you’re well-positioned to connect with both local clients and regional/remote work.
+The business-environment in Cairo (and especially vibrant areas like Nasr City) means being able to highlight local presence, fast communication, and cultural familiarity.</p>
+</div>
+</div>
+    
+<Footer />
 
 
     </> );
